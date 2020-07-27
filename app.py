b8746818fa671441919d5661521eeda39dcf1710
mkdk09 <mkdk099@gmail.com>
2020-07-28 00:02:22 +0900

    wikipedia 追加

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ socketio = SocketIO(app, async_mode=None)
 
 # [削除]value_str = ""
 result = []  # [変更] 結果を格納する配列
-
+wiki_title = ""
 
 @app.route("/")
 def init():
@@ -21,13 +21,17 @@ def init():
 @app.route("/reset", methods=["GET", "POST"])
 def reset_result():
     global result
+    global wiki_title
+    wiki_title = ""
     result = []
-    return render_template('index.html', result=result)
+    return render_template('index.html', result=result, wiki_title=wiki_title)
 
 #  action="/input"
 @app.route("/input", methods=["GET", "POST"])
 def get_form():
     global value_str
+    global wiki_title
+    
     # フォームの値を受け取る
     try:
         value_str = request.form['str']  # name="str"のinputタグの値を取得
@@ -55,15 +59,33 @@ def get_form():
     for sentence in summary:
         result.append(sentences[corpus.index(sentence.__str__())])
 
-    print(sentences)
-    print(corpus)
-    print(summary)
+    # print(sentences)
+    # print(corpus)
+    # print(summary)
     print(result)
 
     # [変更] value_str -> result
     # index.html内にて{{ result }}で挿入できる
-    return render_template('index.html', result=result)
+    return render_template('index.html', result=result, wiki_title=wiki_title)
 
+import requests,bs4
+import wikipedia
+#  action="/wiki"
+@app.route("/wiki", methods=["GET", "POST"])
+def set_wiki():
+    wikipedia.set_lang("ja")
+    url = "https://ja.wikipedia.org/wiki/" + wikipedia.random()
+    res = requests.get(url)
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
+    title = soup.find('title')
+    global wiki_title
+    wiki_title = title.text
+    context = soup.select('.mw-parser-output > p')
+    value_str = ""
+    for c in context:
+        value_str += c.text
+
+    return render_template('index.html', value_str=value_str, wiki_title=wiki_title)
 
 if __name__ == "__main__":
     # debugはデプロイ時にFalseにする
