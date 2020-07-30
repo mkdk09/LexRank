7011d3abbff531e6308757786ec2dde6373e67f9
mkdk09 <mkdk099@gmail.com>
2020-07-31 00:02:22 +0900

    テキストボックスの文字数表示を実装

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,17 @@
+let text_count;
+
 // ウィンドウ読み込み時にフォームにfocusする
 window.onload = function () {
-    document.formStr.str.focus()
+    document.formStr.str.focus();
+    text_count = document.getElementById('textlength');
+    setInterval("countLength(document.formStr.str.value)", 1000);
 }
 
+function countLength(text) {
+    if (text.length !== 0) {
+	text_count.innerHTML = text.length + "文字";
+    }
+    else {
+	text_count.innerHTML = "";
+    }
+}
